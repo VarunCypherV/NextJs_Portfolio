080ee62df37cc95322a7f8946f8fdec87d6a6e7d
VarunCypherV <varunvk2004@gmail.com>
2024-07-08 20:37:00 +0530

    Links added footer

--- src/app/Components/Socials.js
+++ src/app/Components/Socials.js
@@ -6,10 +6,10 @@ import React from 'react'
 function Socials() {
   return (
     <div className="SocialParent">
-      <div className="SocialChild"><Link href=""><img src="/Assets/Footer/Discord.png"/></Link></div>
-      <div className="SocialChild"><Link href=""><img src="/Assets/Footer/Github.png"/></Link></div>
-      <div className="SocialChild"><Link href=""><img src="/Assets/Footer/LinkedIn.png"/></Link></div>
-      <div className="SocialChild"><Link href=""><img src="/Assets/Footer/Instagram.png"/></Link></div>
+      <div className="SocialChild"><Link href="https://discord.gg/5PFnXvZyHR"><img src="/Assets/Footer/Discord.png"/></Link></div>
+      <div className="SocialChild"><Link href="https://github.com/VarunCypherV"><img src="/Assets/Footer/Github.png"/></Link></div>
+      <div className="SocialChild"><Link href="www.linkedin.com/in/varunvetri"><img src="/Assets/Footer/LinkedIn.png"/></Link></div>
+      <div className="SocialChild"><Link href="https://www.instagram.com/dr_instanomics?igsh=MWUzbTZwdGkwc3BnbA=="><img src="/Assets/Footer/Instagram.png"/></Link></div>
     </div>
   )
 }
